000a3262c9aba4f392925312052105f3e8b9e67e
dasaja <danielcj1012@hotmail.com>
2022-10-26 19:13:41 -0500

    Variables en python

---
+++ 01.VARIABLES/app.py
@@ -0,0 +1,24 @@
+print("tipos de datos")
+numero = 10
+nombre = "Daniel"
+precio = 2.99
+acelerar = True
+
+print(type(numero))
+print(type(nombre))
+print(type(precio))
+
+print(type(acelerar))
+
+numero_1 = 100
+numero_2 = 1000
+
+suma = numero_1 + numero_2
+resta = numero_1 - numero_2
+multiplicacion = numero_1 * numero_2
+division = numero_1 / numero_2
+
+print("Suma =", suma)
+print("Resta =", resta)
+print("Multiplicacion =", multiplicacion)
+print("Division =", division)
---
+++ 01.VARIABLES/ejercicio1.py
@@ -0,0 +1,11 @@
+#Promedio General Alumno Daniel
+
+matematicas = 16.5
+lengua = 18.02
+física = 19.3
+Ingles = 14.2
+religion = 11.2
+
+promedioTotal = (matematicas + lengua + fisica + Ingles + religion) / 5
+
+print (promedioTotal)
