04f89c513d6c24781c1899c61ee2df567bb94aea
dasaja <danielcj1012@hotmail.com>
2022-10-28 20:14:07 -0500

    Listas ejercicio 01

---
+++ 05. LISTAS/Ejercicio01.py
@@ -0,0 +1,6 @@
+notas_estudiantes = [9.1, 8.8, 7.5]
+
+suma = sum(notas_estudiantes)
+tamano_elementos = len(notas_estudiantes)
+media = suma/tamano_elementos 
+print(media) #2 decimales
---
+++ 05. LISTAS/Listas.py
@@ -0,0 +1,20 @@
+""" Listas
+=> permiten guardar mas de un elemento dentro de una variable y ademas puede hacerlo con diferentes tipos de datos 
+"""
+
+#Lista vacia 
+lista_vacia = []
+
+#lista con valores
+alumnos = ["Maria","Veronica", "Anai", "Daniel"]
+     
+#acceder alos elementos
+print(alumnos[0]) #muestra 0 elemento
+print(alumnos[1]) #muestra 1 elemento
+print(alumnos[2]) #muestra 1 elemento
+print(alumnos[-1]) #muestra 3 elemento
+
+#cambiar un elemento 
+alumnos[0] = "Elena"
+alumnos[-1] = "Jose"
+print(alumnos)
