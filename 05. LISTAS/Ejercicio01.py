224c82b5a5913e113a9ec0b90891da0be9fca99d
dasaja <danielcj1012@hotmail.com>
2022-10-31 19:04:02 -0500

    Finalización del ejercicio 01

--- 05. LISTAS/Ejercicio01.py
+++ 05. LISTAS/Ejercicio01.py
@@ -4,3 +4,7 @@ suma = sum(notas_estudiantes)
 tamano_elementos = len(notas_estudiantes)
 media = suma/tamano_elementos 
 print(media) #2 decimales
+
+print("4 caracteres, 2 dos decimales :{0:5.2f}".format(media))
+
+
