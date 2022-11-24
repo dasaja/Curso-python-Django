3d97ce9dee7d2a81555abfd383d434f6687af9dd
dasaja <danielcj1012@hotmail.com>
2022-11-23 20:08:35 -0500

    Actualizar repositorio

---
+++ 12. FUNCIONES/Return_print.py
@@ -0,0 +1,14 @@
+#Diferencia entre return y print
+#uso del print 
+def imprimir(a,b):
+  print(a+b)
+
+print(type(imprimir(5,5)))
+
+#return 
+def imprimir2(a,b):
+  return a*b
+
+r = imprimir2(2,4)
+print(r)
+print(type(imprimir2))
--- 12. FUNCIONES/app_funciones.py
+++ 12. FUNCIONES/app_funciones.py
@@ -48,7 +48,7 @@ decir_Adios("Daniel") """
 #crear un return
 #crear funcion sumar
 
-def sumar(num1, num2):
+"""def sumar(num1, num2):
   return num1 + num2 
 
 
@@ -63,5 +63,86 @@ def dividir(num1, num2):
   return num1 / num2
 
 
-resultado = restar(5,5)
-print(resultado)
+resultado = dividir(5,5)
+print(resultado)"""
+
+#Ejemplo 2
+
+"""def verificar_par(numero):
+  return numero % 2 == 0
+
+
+r = verificar_par(2)
+print(r)
+
+r2 = verificar_par(5)
+print(r2)"""
+
+#Ejercicio 3
+"""def verificar_pares_lista(num_list):
+  for number in num_list:
+    if number % 2 == 0:
+      return True 
+    else:
+      pass
+  return False
+
+r = verificar_pares_lista([2,5,6])
+print(r)"""
+
+#Devolver todos los números pares en una lista
+#[1,2,3,4,5,6]
+#Mostrar todos los pares   
+#[2,4,6]
+#Arreglar ejercicio
+"""def verificar_pares_lista(num_list):
+  lista_numeros()
+  for number in num_list:
+    if number % 2 == 0:
+      return number.append(verificar_pares_lista)
+    else:
+      pass
+  
+r = verificar_pares_lista([1,2,3,4,5,6])
+print(r)
+"""
+
+#Ejercicio Profe
+
+"""def check_par_list(num_list):
+    
+    even_numbers = []
+    
+    for number in num_list:
+        if number % 2 == 0:
+            even_numbers.append(number)
+        else:
+            pass
+        
+    return even_numbers
+
+r = check_par_list([1,3,5,2,6,8])
+print(r)"""
+
+
+#Ejercicio profe 2
+"""def f(x): return 2*x**2+1
+
+print(f(2))"""
+
+#Crear dos funciones : 1ra retorne los cuadrados.
+#función 2 que retorne los cubos.
+def f(n):
+  y = n**2
+  return y
+
+def x(n):
+  c = n**3
+  return c
+
+r1 = f(3)
+print(r1)
+
+r2 = x(2)
+print(r2)
+    
