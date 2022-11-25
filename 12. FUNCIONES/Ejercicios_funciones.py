a0ecd5ef5c59bf3e59e37ad608887382557e156b
dasaja <danielcj1012@hotmail.com>
2022-11-24 20:15:53 -0500

    Actualizar Repositorio

--- 12. FUNCIONES/Ejercicios_funciones.py
+++ 12. FUNCIONES/Ejercicios_funciones.py
@@ -65,3 +65,26 @@ def menu():
 
 
 print(menu())
+
+
+
+
+#Ejercicio de función en horas
+#determina cual empleado tiene mas horas de trabajo
+
+horas_trabajas = [('Daniel',200),('Maria',300),('Jose',400),('Elana', 1000)]
+
+def empleado_check(horas_trabajas):
+  max_horas = 0
+  empleado_del_mes = ''
+
+  for empleado, horas in horas_trabajas:
+    if horas>max_horas:
+      max_horas = horas
+      empleado_del_mes = empleado
+    else:
+      pass
+
+  return(empleado_del_mes, max_horas)
+
+print(empleado_check(horas_trabajas))
