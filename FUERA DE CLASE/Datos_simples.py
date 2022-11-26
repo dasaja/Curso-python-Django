3e6382dc177d24d7aa5cfb78814b04c1ed5e1dfc
dasaja <danielcj1012@hotmail.com>
2022-11-26 18:24:36 -0500

    Repasando las clases

---
+++ FUERA DE CLASE/Datos_simples.py
@@ -0,0 +1,24 @@
+#Ejercicio 1
+"""Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!."""
+
+print("¡Hola Mundo!")
+
+#Ejercicio 2 
+"""Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable."""
+
+Variable = "¡Hola Mundo!"
+print(Variable)
+
+#Ejercicio 3
+"""Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido."""
+
+Nombre = input("Cuál es tu nombre?:" "\n")
+print("¡ Hola", Nombre,"!" )
+
+#Ejercicio 4
+"""Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética (3+2/2*5)al cuadrado """
+
+Resultado = (((3+2)/(2*5))**2)
+print(Resultado)
+
+#Ejercicio 5
