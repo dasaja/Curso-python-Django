8650574c46629d6d3476b6ab01442b201b426fd3
dasaja <danielcj1012@hotmail.com>
2022-12-10 16:21:53 -0500

    Actualizar repositorio poofunciones

---
+++ 14. PROGRAMACIÓN ORIENTADA A OBJETOS/poo_clase1.py
@@ -0,0 +1,29 @@
+#Definición de una clase/ Class es un molde para formar los tipos
+#class Carro: #Así se define una class
+lst = [1,2,3,2] #Obejto de tipo lista y el objeto tiene varios métodos, por lo tanto los métodos tienen una funcionalidad
+print(lst.count(2))
+print(type(1))
+
+#Ejercicios 1
+class Carro:
+    pass
+#Instancia o creación de objeto Ej: clase de tipo carro
+ferrari = Carro()
+BMW = Carro()
+print(type(ferrari)) #La clase es un molde del carro
+print(type(BMW))
+
+
+#definicion de una clases
+class Carro:
+  pass
+
+
+#instacia - creacion de objetos
+ferrari = Carro()
+bmw = Carro()
+susuki = Carro()
+
+print(type(ferrari))
+print(type(bmw))
+print(type(susuki))    
